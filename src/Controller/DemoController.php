23d6eaa1804d811379fc1ede416a0fa776d51603
tbtaleb* <tbfeeber@gmail.com>
2024-03-08 09:26:01 +0100

    test

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    #[Route('/{username}', name: 'user_name')]
+    // public function index(): Response
+    // {
+    //     return $this->render('demo/index.html.twig', [
+    //         'controller_name' => 'DemoController',
+    //     ]);
+    // }
+    public function index2($username): Response
+    {
+        return $this->render('demo/index.html.twig', [
+            'username' => $username,
+        ]);
+    }
+
+    #[Route('/test/$age', name: 'test')]
+    public function test($age): Response
+    {
+     return $this->render('demo/test.html.twig', [
+      'title' =>'Les amies',
+     'age' => $age,
+      ]);}
+    
+}
